c5bed19a287cc57707d1a1ee4f215e12069462a0
Louise Mayumi <louise.mayumi@ufms.br>
2026-05-13 19:38:42 -0400

    Add structure and functions for B-tree operations

--- main.c
+++ main.c
@@ -1,1 +1,78 @@
+#include <stdio.h>
+typedef struct no {
 
+"""
+
+# Nome dos integrantes do grupo:
+
+- João VItor Costa Braga - RGA: 2024.1906.XXX-X
+- Louise Mayumi Takigawa Pereia - RGA: 2024.1906.028-5
+
+"""
+
+}
+
+int buscar(no* raiz){
+    """
+    lê uma chave x e realiza sua busca na árvore B, 
+    informando se ela foi encontrada e,
+    quando aplicável, a posição onde se encontra.
+    
+    Entrada:
+
+    Saída:
+    """
+
+};
+
+no* inserir(no* raiz){
+    """
+    lê uma chave x e realiza sua inserção na árvore B.
+
+    Entrada:
+
+    Saída:
+    """
+
+};
+
+void imprimir_arvore(no* raiz){
+
+};
+
+int main() {
+    int resposta = 1;
+    while (resposta){
+
+        printf("// ----- // ----- // ÁRVORE B // ----- // ----- //\n");
+        printf("[1] - Buscar\n");
+        printf("[2] - Inserir\n");
+        printf("[3] - Visualizar\n");
+        printf("[9] - Finalizar\n");
+        if (resposta == 1){
+
+
+        }
+
+        else if (reposta == 2){
+
+
+        }
+
+        else if (resposta == 3){
+
+
+        }
+
+        else if (resposta == 9){
+            printf("\n// ----- Programa finalizado ! ----- //\n");
+        }
+
+        else {
+            printf("Resposta inválida!\n");
+        }
+
+    }
+
+    return 0;
+}
